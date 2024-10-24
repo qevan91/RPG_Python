5909596ba768f6c8e5577f20dfb6ade8a418677a
Ti bolom <noah.severin@ynov.com>
2024-10-24 22:36:17 +0200

    Mise à jour de Position.py

--- Position.py
+++ Position.py
@@ -1,15 +1,37 @@
+from map import Map 
+
 class Position:
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, map_instance):
+        self.map_instance = map_instance
 
-    def Move(self, direction):
-        if direction == "Up":
-            self.y -= 1
-        if direction == "Down":
-            self.y += 1
-        if direction == "Left":
-            self.x -= 1
-        if direction == "Right":
-            self.x += 1
+    def afficher_position(self):
+        print("\n Ou voulez vous aller ? ")
+        print("1. Up")
+        print("2. Down")
+        print("3. Left")
+        print("4. Right")
+
+        reponce = input("> ")
+
+        if reponce == "1":
+            self.map_instance.player_y  -= 1
+            print("le joueur a bouger ")
+        if reponce == "2":
+            self.map_instance.player_y   += 1
+        if reponce == "3":
+            self.map_instance.player_x -= 1
+        if reponce == "4":
+           self.map_instance.player_x  += 1
+
+
+    # def Move(self, direction):
+        
+    #     if direction == "Up":
+    #         self.y -= 1
+    #     if direction == "Down":
+    #         self.y += 1
+    #     if direction == "Left":
+    #         self.x -= 1
+    #     if direction == "Right":
+    #         self.x += 1
